07b626a2f7e177336e9993b18c9234140436f808
Mafid Bin Mizan <116372221+Mafid@users.noreply.github.com>
2024-11-11 16:14:36 +0600

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,36 @@
+// Print something
+console.log("Hello World!");
+
+// Variable
+var price = 12;
+console.log(price);
+console.log(typeof price);
+
+// Find the types of variables
+var name = "Mafid";
+console.log(name);
+console.log(typeof name);
+
+var hot = true;
+console.log(hot);
+console.log(typeof hot);
+
+// 
+var nam = 'Fresh';
+var nam1 = 'Juicy';
+console.log(nam + ' ' + nam1);
+
+var promise = 'I always love you my_Love';
+console.log(promise.toLowerCase());
+console.log(promise.toUpperCase());
+console.log(promise.indexOf('lo'));
+console.log(promise.split(' '));
+
+// Math Function
+var numb = 5;
+console.log(Math.abs(numb));
+console.log(Math.round(numb));
+console.log(Math.ceil(numb));
+console.log(Math.floor(numb));
+console.log(Math.pow(numb, 2));
+console.log(Math.sqrt(numb));
